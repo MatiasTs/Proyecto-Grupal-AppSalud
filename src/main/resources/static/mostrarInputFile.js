4a66da1cac5544b3c1365fb38e3ab74b8eeae27b
“Matias <matias.n.maidana@gmail.com>
2023-12-16 16:54:52 -0300

    Añadiendo boton cambiar imagen y solucionando bugs

---
+++ src/main/resources/static/mostrarInputFile.js
@@ -0,0 +1,9 @@
+
+const buttonFoto = document.getElementById("buttonFoto");
+const imagenInput = document.getElementById("imagenInput");
+
+buttonFoto.addEventListener('click', () => {
+    imagenInput.classList.remove("d-none");
+});
+
+
