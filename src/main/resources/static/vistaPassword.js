9eb0507f42bf86136c3c4763491bfc17dae2f963
“Matias <matias.n.maidana@gmail.com>
2023-12-24 18:28:05 -0300

    Changing image

--- src/main/resources/static/vistaPassword.js
+++ src/main/resources/static/vistaPassword.js
@@ -2,13 +2,15 @@ const imagePassword = document.getElementById("imagePassword"),
         password = document.getElementById("password");
 
 imagePassword.addEventListener('click', () => {
-    if(password.type == "password"){
-        password.type = "text"
-        imagePassword.src = "/images/ocultar.png";
+    if(password.type === "password"){
+        password.type = "text";
+        imagePassword.src = "/images/ocultar-blanco.png";
+        imagePassword.classList.add("masPequenia");
     }else{
         password.type = "password";
         imagePassword.src = "/images/mostrar.jpg";
+        imagePassword.classList.remove("masPequenia");
     }
-})
+});
 
 
