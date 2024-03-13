778767c97e9a88d0f97847afe86bada198c87d17
“Matias <matias.n.maidana@gmail.com>
2024-03-12 21:19:41 -0300

    Simplificando archivo vistaPassword.js

--- target/classes/static/vistaPassword.js
+++ target/classes/static/vistaPassword.js
@@ -1,31 +1,30 @@
 const imagePassword = document.getElementById("imagePassword"),
         password = document.getElementById("password");
 
-imagePassword.addEventListener('click', () => {
-    if(password.type === "password"){
-        password.type = "text";
-        imagePassword.src = "/images/ocultar-blanco.png";
-        imagePassword.classList.add("masPequenia");
-    }else{
-        password.type = "password";
-        imagePassword.src = "/images/mostrar.jpg";
-        imagePassword.classList.remove("masPequenia");
-    }
-});
 
 const imagePassword2 = document.getElementById("imagePassword2"),
         password2 = document.getElementById("password2");
 
-imagePassword2.addEventListener('click', () => {
-    if(password2.type === "password"){
-        password2.type = "text";
-        imagePassword2.src = "/images/ocultar-blanco.png";
-        imagePassword2.classList.add("masPequenia");
-    }else{
-        password2.type = "password";
-        imagePassword2.src = "/images/mostrar.jpg";
-        imagePassword2.classList.remove("masPequenia");
-    }
+imagePassword.addEventListener('click', () => {
+    mostrarOcultarImagen(password, imagePassword);
 });
 
+if(imagePassword2 !== null) {
+    imagePassword2.addEventListener('click', () => {
+        mostrarOcultarImagen(password2, imagePassword2);
+    });
+}
+
+
+function mostrarOcultarImagen(password, image){
+    if(password.type === "password"){
+        password.type = "text";
+        image.src = "/images/ocultar-blanco.png";
+        image.classList.add("masPequenia");
+    }else{
+        password.type = "password";
+        image.src = "/images/mostrar.jpg";
+        image.classList.remove("masPequenia");
+    }
+};
 
